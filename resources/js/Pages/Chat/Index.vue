0ce35f097c7a55a95be376619d2109370a0a2124
Alekseev Konstantin <alexcseev1990@gmail.com>
2024-04-16 08:10:09 +0300

    add main layout and Chat component

---
+++ resources/js/Pages/Chat/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex">
+        <div class="w-1/2 border border-gray-600">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus porro vel tenetur perferendis debitis odio exercitationem ipsum molestiae laborum dicta nesciunt, impedit corrupti aut facilis minima veritatis dolor eveniet quae?
+        </div>
+        <div class="w-1/2 border border-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident libero iusto sequi enim ipsum qui omnis vitae, necessitatibus quod corrupti architecto nisi? Error rerum consectetur explicabo tempore laboriosam facilis delectus.
+        </div>
+    </div>
+   
+</template>
+
+<script>
+import Main from '../../Layouts/Main.vue'
+export default {
+    name: "Index",
+    layout: Main
+} 
+</script>
