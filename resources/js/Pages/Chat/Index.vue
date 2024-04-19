baebe127babdd4f8227b200ad5e7622c232781a2
Alekseev Konstantin <alexcseev1990@gmail.com>
2024-04-19 08:29:37 +0300

    add chat

--- resources/js/Pages/Chat/Index.vue
+++ resources/js/Pages/Chat/Index.vue
@@ -1,12 +1,20 @@
 <template>
     <div class="flex">
         <div class="w-1/2 mr-4 p-4 bg-white border border-gray-200">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus porro vel tenetur perferendis debitis odio exercitationem ipsum molestiae laborum dicta nesciunt, impedit corrupti aut facilis minima veritatis dolor eveniet quae?
+            <h3 class="text-gray-700 mb-4 text-lg">Chats</h3>
+            <div v-if="chats">
+                <div v-for="chat in chats" :key="chat.id" class="flex items-center pb-2 mb-4 border-b border-grey-300">
+                    <Link :href="route('chats.show', chat.id)" class="flex">
+                        <p class="mr-2">{{ chat.id }}</p>
+                        <p>{{ chat.title ?? "Your chat" }}</p>
+                    </Link>
+                </div>
+            </div>
         </div>
         <div class="w-1/2 p-4 bg-white border border-gray-200">
             <h3 class="text-gray-700 mb-4 text-lg">Users</h3>
             <div v-if="users">
-                <div v-for="user in users" class="flex items-center pb-2 mb-4 border-b border-grey-300">
+                <div v-for="user in users" :key="user.id" class="flex items-center pb-2 mb-4 border-b border-grey-300">
                     <p class="mr-2">{{ user.id }}</p>
                     <p class="mr-4">{{ user.name }}</p>
                     <a @click.prevent="store(user.id)" class="inline-block bg-sky-400 text-white text-xs px-3 py-2 rounded-lg " href="#">Message</a>
@@ -18,13 +26,19 @@
 </template>
 
 <script>
-import Main from '../../Layouts/Main.vue'
+import {Link} from '@inertiajs/vue3';
+import Main from '../../Layouts/Main.vue';
 export default {
     name: "Index",
 
     props: [
-        "users"
+        "users",
+        "chats"
     ],
+
+    components: {
+        Link,
+    },
 
     layout: Main,
 
